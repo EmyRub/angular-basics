c38b05bb7dd63797385ccf44a0e381722d7e2ab2
EmyRub <esme.rubi@hotmail.com>
2022-10-17 14:59:51 -0500

    inicio para producción

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PersonajesComponent } from './personajes/personajes.component';
 import { AgregarComponent } from './agregar/agregar.component';
+import { dbzService } from './services/dbz.service';
 
 
 
@@ -20,6 +21,9 @@ import { AgregarComponent } from './agregar/agregar.component';
   ],
   exports: [
     MainPageComponent
+  ],
+  providers: [
+    dbzService
   ]
 })
 export class DbzModule { }
